70a2288a76bbf495eeb5e9f1fd65dfadec8a1adf
Laisa Pereira <laisasilva.pereira@yahoo.com>
2022-06-24 15:30:36 -0300

    colocando cores q serao utilizadas no tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,14 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
-import { client } from "./lib/apollo"
 
-const GET_LESSONS_QUERY = gql`
-    query {
-      lessons {
-        id
-        title
+import Event from "./pages/Event"
 
-        teacher{
-          name
-        }
-      }
-    }
-`
 
 function App() {
- /*  useEffect(() => {
-    client.query({
-      query: GET_LESSONS_QUERY,
-    }).then(response => {
-      console.log(response.data)
-    })
-  }, [])
-   */
+ 
 
-  const { data } = useQuery(GET_LESSONS_QUERY)
-
-  console.log(data)
 
   return (
     <div>
-      <h1 className="text-5xl">oioioi</h1>
+      <Event />
     </div>
   )
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { Logo } from "./Logo";
+
+import './HeaderStyle.css'
+
+export default function Header() {
+  return (
+    <header>
+      <Logo/>
+    </header>
+  )
+}
