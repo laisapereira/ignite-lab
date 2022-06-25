cad39333db33477f39cfaf56a3dd4e860e6f81ab
Laisa Pereira <laisasilva.pereira@yahoo.com>
2022-06-25 15:21:35 -0300

    video adicionado

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -1,11 +1,18 @@
+import { DefaultUi, Player, Youtube } from '@vime/react'
 import { CaretRight, DiscordLogo, FileArrowDown, ImageSquare, Lightning } from 'phosphor-react'
-import React from 'react'
+
+import '@vime/core/themes/default.css'
 
 export default function Video() {
   return (
     <div className='flex-1'>
       <div className='bg-black flex justify-center'>
-        <div className='h-full w-full max-w-[1100px] max-h-[60vh] aspect-video'></div>
+        <div className='h-full w-full max-w-[1100px] max-h-[60vh] aspect-video'>
+          <Player>
+            <Youtube videoId='cUT665tW4v8' />
+            <DefaultUi />
+          </Player>
+        </div>
       </div>
 
       <div className='p-8 max-w-[1100px] mx-auto'>
