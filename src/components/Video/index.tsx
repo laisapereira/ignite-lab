54d88ffa35dc12ef01dafb010aaa722ac606c6ec
Laisa Pereira <laisasilva.pereira@yahoo.com>
2022-06-25 15:03:25 -0300

    estatico da parte do vídeo pronto

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -1,4 +1,4 @@
-import { DiscordLogo, Lightning } from 'phosphor-react'
+import { CaretRight, DiscordLogo, FileArrowDown, ImageSquare, Lightning } from 'phosphor-react'
 import React from 'react'
 
 export default function Video() {
@@ -22,11 +22,13 @@ export default function Video() {
 
 
             <div className='flex items-center gap-4 mt-6'>
-              <img 
-              className='h-16 w-16 rounded-full border-2 border-blue-500'
-              src="https://github.com/laisapereira.png" 
-              alt=""
-              />
+              <a href="https://github.com/laisapereira">
+                <img
+                className='h-16 w-16 rounded-full border-2 border-blue-500'
+                src="https://github.com/laisapereira.png"
+                alt=""
+                />
+              </a>
               <div>
                 <strong className='font-bold text-2xl block'> Laisa Pereira</strong>
                 <span className='text-gray-200 text-sm block'> Dev Front-End</span>
@@ -44,6 +46,38 @@ export default function Video() {
                 Acesse o desafio
             </a>
           </div>
+        </div>
+
+        <div className='gap-8 mt-20 grid grid-cols-2'>
+          <a href="" className='bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors'>
+            <div className= "flex items-center bg-green-700 h-full p-6">
+              <FileArrowDown size={40}/>
+            </div>
+            <div className='py-6 leading-relaxed'>
+              <strong className='text-2xl'> Material Complementar</strong>
+              <p className='text-sm text-gray-200 mt-2'>
+              Acesse o material complementar para acelerar o seu desenvolvimento
+              </p>
+            </div>
+            <div className='h-full p-6 flex items-center'>
+              <CaretRight size={24}/>
+            </div>
+          </a>
+          
+          <a href="" className='bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors'>
+          <div className='bg-green-700 h-full p-6 flex items-center'>
+              <ImageSquare size={40}/>
+            </div>
+            <div className='py-6 leading-relaxed'>
+              <strong className='text-2xl'> Wallpapers Exclusivos</strong>
+              <p className='text-sm text-gray-200 mt-2'>
+              Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina
+              </p>
+            </div>
+            <div className='h-full p-6 flex items-center'>
+              <CaretRight size={24}/>
+            </div>
+          </a>
         </div>
         <div>
 
